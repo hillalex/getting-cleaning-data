8428d70fda0d6b01d43047c396a9181f13bb394e
Alex <alex.hill@gmail.com>
2020-10-11 14:52:36 +0100

    create tidy data set

---
+++ downloadData.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -ex
+
+wget https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip -O temp.zip
+unzip temp.zip
+rm temp.zip
+
+mv 'UCI HAR Dataset' source-data
---
+++ run_analysis.R
@@ -0,0 +1,55 @@
+library(readr)
+library(dplyr)
+
+if (!dir.exists("output-data")) {
+  dir.create("output-data")
+}
+
+read_data_file <- function(file_path) {
+  read_delim(paste0("source-data/", file_path), delim = " ", col_names = FALSE, trim_ws = TRUE)
+}
+
+# merge test and train data
+X_test <- read_data_file("test/X_test.txt")
+y_test <- read_data_file("test/y_test.txt")
+
+X_train <- read_data_file("train/X_train.txt")
+y_train <- read_data_file("train/y_train.txt")
+
+X_total <- rbind(X_test, X_train)
+y_total <- rbind(y_test, y_train)
+
+subject_test <- read_data_file("test/subject_test.txt")
+subject_train <- read_data_file("train/subject_train.txt")
+
+subject_total <- rbind(subject_test, subject_train)
+
+# extract mean and standard deviation measurements
+features <- read_data_file("features.txt")
+filtered_features <- features %>%
+  filter(grepl("((.*)mean(.*))|((.*)std(.*))", X2))
+
+filtered_feature_cols_vector <- filtered_features[["X1"]]
+
+data <- X_total %>%
+  select(filtered_feature_cols_vector)
+
+# label activities
+activity_labels <- read_data_file("activity_labels.txt")
+activity_factor <- as.factor(activity_labels[[2]])
+
+y_total_labelled <- y_total %>% mutate(activity = as.factor(X1)) %>%
+ select(activity)
+
+levels(y_total_labelled[["activity"]]) <- levels(activity_factor)
+data <- cbind(y_total_labelled, data)
+
+# add subjects
+data <- cbind(y_total, data)
+names(data) <- c("subject", "activity", filtered_features[["X2"]])
+
+# write data set to file
+write.table(data, file="output-data/data.txt", row.names = FALSE)
+
+# calc average of each variable for each activity and subject
+
